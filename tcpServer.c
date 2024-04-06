422f5787849237f91da7bf6403ac025a4bbffdd8
suttha <cn352@suttha>
2024-04-06 11:07:12 +0700

    change some permission

--- tcpServer.c
+++ tcpServer.c
@@ -10,9 +10,8 @@
 #include <stdio.h>
 #include <unistd.h> /* close */
 
-
 #define SUCCESS 0
-#define ERROR   1
+#define ERROR 1
 
 #define END_LINE 0x0
 #define SERVER_PORT 15121
@@ -21,61 +20,68 @@
 /* function readline */
 int read_line();
 
-int main (int argc, char *argv[]) {
-  
+int main(int argc, char *argv[])
+{
+
   int sd, newSd, cliLen;
 
   struct sockaddr_in cliAddr, servAddr;
   char line[MAX_MSG];
 
-
   /* create socket */
   sd = socket(AF_INET, SOCK_STREAM, 0);
-   if(sd<0) {
+  if (sd < 0)
+  {
     perror("cannot open socket ");
     return ERROR;
   }
-  
+
   /* bind server port */
   servAddr.sin_family = AF_INET;
   servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
   servAddr.sin_port = htons(SERVER_PORT);
-  
-  if(bind(sd, (struct sockaddr *) &servAddr, sizeof(servAddr))<0) {
+
+  if (bind(sd, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0)
+  {
     perror("cannot bind port ");
     return ERROR;
   }
 
-  listen(sd,5);
-  
-  while(1) {
+  listen(sd, 5);
 
-    printf("%s: waiting for data on port TCP %u\n",argv[0],SERVER_PORT);
+  while (1)
+  {
+
+    printf("%s: waiting for data on port TCP %u\n", argv[0], SERVER_PORT);
 
     cliLen = sizeof(cliAddr);
-    newSd = accept(sd, (struct sockaddr *) &cliAddr, &cliLen);
-    if(newSd<0) {
+    newSd = accept(sd, (struct sockaddr *)&cliAddr, &cliLen);
+    if (newSd < 0)
+    {
       perror("cannot accept connection ");
       return ERROR;
     }
-    
-    /* init line */
-    memset(line,0x0,MAX_MSG);
-    
-    /* receive segments */
-    while(read_line(newSd,line)!=ERROR) {
-      
-      printf("%s: received from %s:TCP%d : %s\n", argv[0], 
-	     inet_ntoa(cliAddr.sin_addr),
-	     ntohs(cliAddr.sin_port), line);
-      /* init line */
-      memset(line,0x0,MAX_MSG);
-      
-    } /* while(read_line) */
-    
-  } /* while (1) */
 
+    /* init line */
+    memset(line, 0x0, MAX_MSG);
+
+    /* receive segments */
+    while (read_line(newSd, line) != ERROR)
+    {
+
+      printf("%s: received from %s:TCP%d : %s\n", argv[0],
+             inet_ntoa(cliAddr.sin_addr),
+             ntohs(cliAddr.sin_port), line);
+      /* init line */
+      memset(line, 0x0, MAX_MSG);
+
+    } /* while(read_line) */
+
+  } /* while (1) */
 }
+
+
+
 
 
 /* WARNING WARNING WARNING WARNING WARNING WARNING WARNING       */
@@ -90,64 +96,72 @@ int main (int argc, char *argv[]) {
 /* each received byte.                                                   */
 /* You can set END_CHAR to whatever means endofline for you. (0x0A is \n)*/
 /* read_lin returns the number of bytes returned in line_to_return       */
-int read_line(int newSd, char *line_to_return) {
-  
-  static int rcv_ptr=0;
+int read_line(int newSd, char *line_to_return)
+{
+
+  static int rcv_ptr = 0;
   static char rcv_msg[MAX_MSG];
   static int n;
   int offset;
 
-  offset=0;
+  offset = 0;
 
-  while(1) {
-    if(rcv_ptr==0) {
+  while (1)
+  {
+    if (rcv_ptr == 0)
+    {
       /* read data from socket */
-      memset(rcv_msg,0x0,MAX_MSG); /* init buffer */
+      memset(rcv_msg, 0x0, MAX_MSG);        /* init buffer */
       n = recv(newSd, rcv_msg, MAX_MSG, 0); /* wait for data */
-      if (n<0) {
-	perror(" cannot receive data ");
-	return ERROR;
-      } else if (n==0) {
-	printf(" connection closed by client\n");
-	close(newSd);
-	return ERROR;
+      if (n < 0)
+      {
+        perror(" cannot receive data ");
+        return ERROR;
+      }
+      else if (n == 0)
+      {
+        printf(" connection closed by client\n");
+        close(newSd);
+        return ERROR;
       }
     }
-  
+
     /* if new data read on socket */
     /* OR */
     /* if another line is still in buffer */
 
     /* copy line into 'line_to_return' */
-    while(*(rcv_msg+rcv_ptr)!=END_LINE && rcv_ptr<n) {
-      memcpy(line_to_return+offset,rcv_msg+rcv_ptr,1);
+    while (*(rcv_msg + rcv_ptr) != END_LINE && rcv_ptr < n)
+    {
+      memcpy(line_to_return + offset, rcv_msg + rcv_ptr, 1);
       offset++;
       rcv_ptr++;
     }
-    
+
     /* end of line + end of buffer => return line */
-    if(rcv_ptr==n-1) { 
+    if (rcv_ptr == n - 1)
+    {
       /* set last byte to END_LINE */
-      *(line_to_return+offset)=END_LINE;
-      rcv_ptr=0;
+      *(line_to_return + offset) = END_LINE;
+      rcv_ptr = 0;
       return ++offset;
-    } 
-    
+    }
+
     /* end of line but still some data in buffer => return line */
-    if(rcv_ptr <n-1) {
+    if (rcv_ptr < n - 1)
+    {
       /* set last byte to END_LINE */
-      *(line_to_return+offset)=END_LINE;
+      *(line_to_return + offset) = END_LINE;
       rcv_ptr++;
       return ++offset;
     }
 
     /* end of buffer but line is not ended => */
     /*  wait for more data to arrive on socket */
-    if(rcv_ptr == n) {
+    if (rcv_ptr == n)
+    {
       rcv_ptr = 0;
-    } 
-    
+    }
+
   } /* while */
 }
-  
-  
