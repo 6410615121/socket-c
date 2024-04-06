af1d9a27a0f5f999accc1bd7fbd52e91c358ba46
suttha <cn352@suttha>
2024-04-06 22:51:45 +0700

    move read part to read_client()

--- hw4_server.c
+++ hw4_server.c
@@ -9,6 +9,7 @@
 #include <netdb.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <unistd.h> /* close */
 
 #define SUCCESS 0
@@ -19,15 +20,18 @@
 #define MAX_MSG 100
 
 /* function readline */
-int read_line();
+char *read_client(int newSd);
 
 int main(int argc, char *argv[])
 {
 
-    int sd, newSd, cliLen;
+    int sd, newSd, cliLen, rc;
 
     struct sockaddr_in cliAddr, servAddr;
     char line[MAX_MSG];
+
+    char recvBuff[1024];
+    char sendBuff[1024];
 
     /* create socket */
     sd = socket(AF_INET, SOCK_STREAM, 0);
@@ -72,25 +76,12 @@ int main(int argc, char *argv[])
         }
 
         // read from client //
-        char recvBuff[1024];
-        memset(recvBuff, 0, sizeof(recvBuff));
+        strcpy(recvBuff, read_client(newSd)); // call read_client() which call read()
+        printf("%s: received from %s:TCP%d : %s\n", argv[0],
+        inet_ntoa(cliAddr.sin_addr),
+        ntohs(cliAddr.sin_port), recvBuff);
 
-        int rc = read(newSd, recvBuff, sizeof(recvBuff));
-        if (rc < 0)
-        {
-            perror("Error reading from socket");
-        }
-        else if (rc > 0)
-        {
-            // Null-terminate
-            recvBuff[rc] = '\0';
-
-            printf("%s: received from %s:TCP%d : %s\n", argv[0],
-                   inet_ntoa(cliAddr.sin_addr),
-                   ntohs(cliAddr.sin_port), recvBuff);
-        }
-
-        // keep 
+        // keep name and birthdate in tokens
         char *token = strtok(recvBuff, " ");
         char *tokens[10];
         int token_count = 0;
@@ -103,12 +94,11 @@ int main(int argc, char *argv[])
 
         // print name and surname
         printf("name: %s %s\n", tokens[0], tokens[1]);
-
+        
         // end read from client //
 
 
         // send response to client //
-        char sendBuff[1024];
         memset(sendBuff, 0, sizeof(sendBuff));
 
         strcpy(sendBuff, "Server recieved");
@@ -121,84 +111,32 @@ int main(int argc, char *argv[])
     } /* while (1) */
 }
 
-/* WARNING WARNING WARNING WARNING WARNING WARNING WARNING       */
-/* this function is experimental.. I don't know yet if it works  */
-/* correctly or not. Use Steven's readline() function to have    */
-/* something robust.                                             */
-/* WARNING WARNING WARNING WARNING WARNING WARNING WARNING       */
 
-/* rcv_line is my function readline(). Data is read from the socket when */
-/* needed, but not byte after bytes. All the received data is read.      */
-/* This means only one call to recv(), instead of one call for           */
-/* each received byte.                                                   */
-/* You can set END_CHAR to whatever means endofline for you. (0x0A is \n)*/
-/* read_lin returns the number of bytes returned in line_to_return       */
-int read_line(int newSd, char *line_to_return)
-{
 
-    static int rcv_ptr = 0;
-    static char rcv_msg[MAX_MSG];
-    static int n;
-    int offset;
+char *read_client(int newSd) {
+    char recvBuff[1024];
+    memset(recvBuff, 0, sizeof(recvBuff));
 
-    offset = 0;
-
-    while (1)
-    {
-        if (rcv_ptr == 0)
-        {
-            /* read data from socket */
-            memset(rcv_msg, 0x0, MAX_MSG);        /* init buffer */
-            n = recv(newSd, rcv_msg, MAX_MSG, 0); /* wait for data */
-            if (n < 0)
-            {
-                perror(" cannot receive data ");
-                return ERROR;
-            }
-            else if (n == 0)
-            {
-                printf(" connection closed by client\n");
-                close(newSd);
-                return ERROR;
-            }
+    int rc = read(newSd, recvBuff, sizeof(recvBuff) - 1);
+    if (rc < 0) {
+        perror("Error reading from socket");
+        return NULL;
+    } else if (rc == 0) {
+        printf("Connection closed by client\n");
+        return NULL;
+    } else {
+        // Allocate memory for the received string (including null terminator)
+        char *received_data = malloc(rc + 1);
+        if (received_data == NULL) {
+            perror("Failed to allocate memory for received data");
+            return NULL;
         }
-
-        /* if new data read on socket */
-        /* OR */
-        /* if another line is still in buffer */
-
-        /* copy line into 'line_to_return' */
-        while (*(rcv_msg + rcv_ptr) != END_LINE && rcv_ptr < n)
-        {
-            memcpy(line_to_return + offset, rcv_msg + rcv_ptr, 1);
-            offset++;
-            rcv_ptr++;
-        }
-
-        /* end of line + end of buffer => return line */
-        if (rcv_ptr == n - 1)
-        {
-            /* set last byte to END_LINE */
-            *(line_to_return + offset) = END_LINE;
-            rcv_ptr = 0;
-            return ++offset;
-        }
-
-        /* end of line but still some data in buffer => return line */
-        if (rcv_ptr < n - 1)
-        {
-            /* set last byte to END_LINE */
-            *(line_to_return + offset) = END_LINE;
-            rcv_ptr++;
-            return ++offset;
-        }
-
-        /* end of buffer but line is not ended => */
-        /*  wait for more data to arrive on socket */
-        if (rcv_ptr == n)
-        {
-            rcv_ptr = 0;
-        }
-
-    } /* while */
+        
+        // Copy the received data into the dynamically allocated memory
+        memcpy(received_data, recvBuff, rc);
+        received_data[rc] = '\0'; // Null-terminate the string
+        
+        return received_data;
+    }
 }
+
